cf41ac17120436023b34826205519c4e406be436
Chintan0607 <103286240+Chintan0607@users.noreply.github.com>
2024-12-17 12:36:45 +0530

    Update app.js

--- Backend/fedextrackexpressjs/app.js
+++ Backend/fedextrackexpressjs/app.js
@@ -1,18 +1,24 @@
-const dotenv = require('dotenv');
-dotenv.config()
-const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
 const web = require("./routes/web.js");
-const cors = require('cors');
-const app = express()
+const cors = require("cors");
+
+const app = express();
 app.use(cors());
-const port = process.env.PORT || '3000'
+const port = process.env.PORT || "3000";
 
 // JSON
-app.use(express.json())
+app.use(express.json());
+
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Welcome to FedEx Track Express API!");
+});
 
 // Load Routes
-app.use("/fedex", web)
+app.use("/fedex", web);
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
-})
+  console.log(`Server listening at http://localhost:${port}`);
+});
