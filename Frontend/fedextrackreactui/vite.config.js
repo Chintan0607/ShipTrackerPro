bf49abdcbcd5e9ceadc5033bff91e6f6ae6ffc9c
Chintan0607 <103286240+Chintan0607@users.noreply.github.com>
2024-12-17 16:14:51 +0530

    Update vite.config.js

--- Frontend/fedextrackreactui/vite.config.js
+++ Frontend/fedextrackreactui/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 10000,  // Use the environment variable PORT or fallback to 10000
+    host: '0.0.0.0'  // Ensure the server binds to all network interfaces
+  }
 })
